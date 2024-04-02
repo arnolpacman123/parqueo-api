7c03d1923142a8e7462e7e9467ebf15bcfabc613
Arnol Guevara <arnolpacman@gmail.com>
2024-04-02 12:30:46 -0400

    updated

---
+++ src/maps/models/interfaces/map-tile.interface.ts
@@ -0,0 +1,4 @@
+export interface MapTile {
+    name: string;
+    url: string;
+}
--- src/maps/services/map-tiles.service.ts
+++ src/maps/services/map-tiles.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { MapTile } from '../models/interfaces/map-tile.interface';
 
 @Injectable()
 export class MapTileService {
-    mapTiles = [
-        'https://api.maptiler.com/maps/openstreetmap/style.json?key=gstc1HWmTnndFXytaloa',
-    ];
+  mapTiles: MapTile[] = [
+    {
+      name: 'OpenStreetMap',
+      url: 'https://api.maptiler.com/maps/openstreetmap/style.json?key=gstc1HWmTnndFXytaloa',
+    },
+  ];
 
-    getMapTiles() {
-        return this.mapTiles;
-    }
+  getMapTiles() {
+    return this.mapTiles;
+  }
 }
