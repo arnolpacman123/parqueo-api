ea37c7bd34409fa14ea1c3afe584a8c5039d963c
Arnol Guevara <arnolpacman@gmail.com>
2024-04-02 12:52:32 -0400

    updated

--- src/maps/services/map-tiles.service.ts
+++ src/maps/services/map-tiles.service.ts
@@ -8,6 +8,18 @@ export class MapTileService {
       name: 'OpenStreetMap',
       url: 'https://api.maptiler.com/maps/openstreetmap/style.json?key=gstc1HWmTnndFXytaloa',
     },
+    {
+      name: 'MapTiler Basic',
+      url: 'https://api.maptiler.com/maps/basic-v2/style.json?key=gstc1HWmTnndFXytaloa',
+    },
+    {
+      name: 'MapTiler Streets',
+      url: 'https://api.maptiler.com/maps/streets-v2/style.json?key=gstc1HWmTnndFXytaloa',
+    },
+    {
+      name: 'MapTiler Topo',
+      url: 'https://api.maptiler.com/maps/topo-v2/style.json?key=gstc1HWmTnndFXytaloa',
+    },
   ];
 
   getMapTiles() {
