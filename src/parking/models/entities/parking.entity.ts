35404335cb4cba7c7395ef69f897c77a0c1b2906
Arnol Guevara <arnolpacman@gmail.com>
2024-04-22 14:01:10 -0400

    updated

--- src/parking/models/entities/parking.entity.ts
+++ src/parking/models/entities/parking.entity.ts
@@ -1,41 +1,65 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Point } from "geojson";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Point } from 'geojson';
 
 @Entity({
-  name: "parking"
+  name: 'parking',
 })
 export class Parking {
   @PrimaryGeneratedColumn({
-    name: "id"
+    name: 'id',
   })
   id?: number;
 
   @Column({
-    name: "geom",
-    type: "geometry",
-    spatialFeatureType: "Point",
-    srid: 4326
+    name: 'geom',
+    type: 'geometry',
+    spatialFeatureType: 'Point',
+    srid: 4326,
   })
   geom?: Point;
 
   @Column({
-    name: "is_full",
-    type: "boolean"
+    name: 'is_full',
+    type: 'boolean',
   })
   isFull?: boolean;
 
+  @Column({
+    name: 'start_attention',
+    type: 'text',
+  })
+  startAttention: string;
+
+  @Column({
+    name: 'end_attention',
+    type: 'text',
+  })
+  endAttention: string;
+
+  @Column({
+    name: 'image_url',
+    type: 'text',
+  })
+  imageUrl: string;
+
   @CreateDateColumn({
-    name: "created_at",
-    type: "timestamp",
-    default: () => "CURRENT_TIMESTAMP"
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
   })
   createdAt?: Date;
 
   @UpdateDateColumn({
-    name: "updated_at",
-    type: "timestamp",
-    default: () => "CURRENT_TIMESTAMP",
-    onUpdate: "CURRENT_TIMESTAMP"
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
   })
   updatedAt?: Date;
 }
